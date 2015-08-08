32047611d1df8c4195a4f7fe4025abe02203f17a
Mila Frerichs <mila.frerichs@gmail.com>
2015-08-08 17:40:01 +0200

    Initial middleman template

---
+++ config.rb
@@ -0,0 +1,42 @@
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+activate :sprockets
+sprockets.append_path File.join "#{root}", "bower_components"
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+activate :deploy do |deploy|
+  deploy.build_before = true # default: false
+  deploy.method = :git
+end
+activate :relative_assets
+set :relative_links, true
---
+++ source/javascripts/all.js
@@ -0,0 +1,12 @@
+//= require d3
+//= require jquery
+//= require topojson
+//= require queue-async
+//= require colorbrewer
+//= require underscore
+//= require vega
+//= require handlebars
+//= require d3graph
+//= require stacked
+//= require_tree .
+
